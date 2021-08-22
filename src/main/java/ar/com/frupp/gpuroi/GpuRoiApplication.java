ed685c11348a8a6b00ce8906ccc7d9158daaaf93
Franco Nahuel Rupp <ruppfn@gmail.com>
2021-08-22 18:07:08 -0300

    first commit from Initializr

---
+++ src/main/java/ar/com/frupp/gpuroi/GpuRoiApplication.java
@@ -0,0 +1,13 @@
+package ar.com.frupp.gpuroi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpuRoiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GpuRoiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ar/com/frupp/gpuroi/GpuRoiApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.frupp.gpuroi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpuRoiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
