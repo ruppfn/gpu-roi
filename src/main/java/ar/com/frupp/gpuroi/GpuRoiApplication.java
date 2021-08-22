2d043c74b3f967bf873aae9c5596ee7980bfbb8f
Franco Nahuel Rupp <ruppfn@gmail.com>
2021-08-22 19:06:02 -0300

    add RestTemplate bean

--- src/main/java/ar/com/frupp/gpuroi/GpuRoiApplication.java
+++ src/main/java/ar/com/frupp/gpuroi/GpuRoiApplication.java
@@ -2,6 +2,9 @@ package ar.com.frupp.gpuroi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class GpuRoiApplication {
@@ -10,4 +13,8 @@ public class GpuRoiApplication {
         SpringApplication.run(GpuRoiApplication.class, args);
     }
 
+    @Bean
+    public RestTemplate restTemplateBean(RestTemplateBuilder builder) {
+        return builder.build();
+    }
 }
