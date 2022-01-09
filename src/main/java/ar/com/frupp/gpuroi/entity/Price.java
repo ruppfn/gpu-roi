e241310c50c9377bfd5da84c9209241d7d35c672
Franco Nahuel Rupp <ruppfn@gmail.com>
2022-01-09 10:02:04 -0300

    add lombok constructors to Price entity

--- src/main/java/ar/com/frupp/gpuroi/entity/Price.java
+++ src/main/java/ar/com/frupp/gpuroi/entity/Price.java
@@ -1,6 +1,8 @@
 package ar.com.frupp.gpuroi.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
 import javax.persistence.Entity;
@@ -10,6 +12,7 @@ import java.math.BigDecimal;
 
 @Entity @Table(name = "PRICES")
 @Getter
+@AllArgsConstructor @NoArgsConstructor
 public class Price {
 
     public static final String USD_NAME = "USD";
