fe31fed703e80e421d04da1d94f7b392276640fd
Franco Nahuel Rupp <ruppfn@gmail.com>
2022-01-09 10:07:55 -0300

    add usd price to the scheduler

--- src/main/java/ar/com/frupp/gpuroi/scheduler/SyncScheduler.java
+++ src/main/java/ar/com/frupp/gpuroi/scheduler/SyncScheduler.java
@@ -1,20 +1,25 @@
 package ar.com.frupp.gpuroi.scheduler;
 
 import ar.com.frupp.gpuroi.service.DeviceService;
+import ar.com.frupp.gpuroi.service.PriceService;
+import lombok.AllArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
+@AllArgsConstructor
 public class SyncScheduler {
 
     private final DeviceService deviceService;
-
-    public SyncScheduler(DeviceService deviceService) {
-        this.deviceService = deviceService;
-    }
+    private final PriceService priceService;
 
     @Scheduled(cron = "@hourly")
     public void syncDevices() {
         this.deviceService.sync();
+    }
+
+    @Scheduled(cron = "@hourly")
+    public void syncUsdPrice() {
+        this.priceService.sync();
     }
 }
