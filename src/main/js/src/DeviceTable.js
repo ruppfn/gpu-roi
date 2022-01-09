e4acc7951e5d0333eed135220f9dbbeaaa710512
Franco Nahuel Rupp <ruppfn@gmail.com>
2022-01-09 06:03:03 -0300

    add possibility to update the price of a device

--- src/main/js/src/DeviceTable.js
+++ src/main/js/src/DeviceTable.js
@@ -1,11 +1,16 @@
-import React, {useEffect, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import {DataGrid} from "@mui/x-data-grid";
+import Snackbar from '@mui/material/Snackbar';
+import Alert from '@mui/material/Alert';
 import axios from "axios";
 
 function DeviceTable() {
 
     const URL = "/api/devices";
     // const URL = "http://localhost:8080/api/devices";
+
+    const [snackbar, setSnackbar] = useState(null);
+    const handleCloseSnackbar = () => setSnackbar(null);
 
     const [devices, setDevices] = useState([]);
     const [pageNumber, setPageNumber] = useState(0);
@@ -18,6 +23,34 @@ function DeviceTable() {
     ];
 
     const pageSize = 10;
+
+    const handleRowChange = useCallback(
+        async (params) => {
+
+            const updatePrice = async (deviceId, price) => {
+                const request = {
+                    deviceId, price
+                };
+                const response = await axios.put(URL, request).then(response => response.data);
+                console.log("Update Price response: ", response);
+                return response;
+            };
+
+            try {
+                const response = await updatePrice(params.id, params.value);
+
+                setSnackbar({ children: 'Price successfully saved', severity: 'success' });
+                setDevices((prev) =>
+                    prev.map((row) => (row.id === params.id ? { ...row, ...response } : row)),
+                );
+            } catch (error) {
+                setSnackbar({ children: 'Error while saving price', severity: 'error' });
+                // Restore the row in case of error
+                setDevices((prev) => [...prev]);
+            }
+        },
+        [setDevices],
+    );
 
     useEffect(() => {
         axios.get(URL, {
@@ -35,7 +68,7 @@ function DeviceTable() {
         {field: "name", headerName: "Name", flex: 4},
         {field: "power", headerName: "Power", flex: 1, renderCell: cell => powerFormat(cell) },
         {field: "paying", headerName: "BTC per Day", flex: 2},
-        {field: "priceInArs", headerName: "Price in ARS", flex: 1},
+        {field: "priceInArs", headerName: "Price in ARS", flex: 1, editable: true},
         {field: "daysToROI", headerName: "Days to ROI", flex: 1}
     ];
 
@@ -55,8 +88,14 @@ function DeviceTable() {
                     page={pageNumber}
                     onPageChange={newPageNumber => setPageNumber(newPageNumber)}
                     sortModel={sortModel}
+                    onCellEditCommit={handleRowChange}
                 />
             </div>
+            {!!snackbar && (
+                <Snackbar open onClose={handleCloseSnackbar} autoHideDuration={6000}>
+                    <Alert {...snackbar} onClose={handleCloseSnackbar} />
+                </Snackbar>
+            )}
         </div>
     );
 }
