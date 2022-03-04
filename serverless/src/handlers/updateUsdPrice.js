ca57574506e8f52a63f9bfc4453e4e9db21c5512
Franco Nahuel Rupp <franco.rupp@avalith.net>
2022-03-04 16:15:55 -0300

    move business logic out of updateUsdPrice handler

--- serverless/src/handlers/updateUsdPrice.js
+++ serverless/src/handlers/updateUsdPrice.js
@@ -1,9 +1,6 @@
 "use strict";
 
-const AWS = require("aws-sdk");
-const { getUsdPrice } = require("../useCases/getUsdPrice");
-
-const dynamo = new AWS.DynamoDB.DocumentClient();
+const { updateUsdPrice } = require("../useCases/updateUsdPrice");
 
 const USD_URL = process.env.USD_URL || "";
 
@@ -11,26 +8,12 @@ console.log(`USD_URL: ${USD_URL}`);
 
 module.exports.handler = async () => {
 
-    const price = await getUsdPrice(USD_URL);
-
-    const priceItem = {
-        Type: "USD",
-        Price: price
-    };
-
-    console.log(priceItem);
-
-    await dynamo.put({
-        TableName: "Prices",
-        Item: priceItem
-    }).promise();
+    await updateUsdPrice(USD_URL);
 
     return {
         statusCode: 200,
         body: JSON.stringify(
-            {
-                price: price
-            },
+            null,
             null,
             2
         ),
