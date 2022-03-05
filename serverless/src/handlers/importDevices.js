66b4b8d9961bdf06c1d1f4f179a2c13df031134d
Franco Nahuel Rupp <franco.rupp@avalith.net>
2022-03-04 21:11:16 -0300

    move business logic out of importDevices handler

--- serverless/src/handlers/importDevices.js
+++ serverless/src/handlers/importDevices.js
@@ -1,49 +1,11 @@
 "use strict";
 
-const axios = require("axios");
-const AWS = require("aws-sdk");
-
-const dynamo = new AWS.DynamoDB.DocumentClient();
-
+const { importDevices } = require("../useCases/importDevices");
 const DEVICES_URL = process.env.DEVICES_URL || "";
-
-const BATCH_SIZE = 25;
 
 module.exports.handler = async () => {
 
-    const res = await axios.get(DEVICES_URL);
-
-    const {devices} = res.data;
-
-    const gpus = devices.filter(device => device.category !== "ASIC" && device.paying > 0);
-
-    const convertedGpus = gpus.map(gpu => convertGpuToRequiredObject(gpu));
-
-    const promises = []
-    let items = [];
-    let batchNumber = 0;
-
-    for (const gpu of convertedGpus) {
-        items.push(gpu);
-
-        if (items.length === BATCH_SIZE) {
-            console.log(`Saving Batch ${batchNumber}`);
-            promises.push(saveToDynamoDb(items));
-
-            batchNumber++;
-            items = [];
-        }
-    }
-
-    if (items.length > 0) {
-        console.log(`Saving Batch ${batchNumber}`);
-        promises.push(saveToDynamoDb(items));
-    }
-
-    if (promises.length > 0) {
-        console.log(`awaiting ${promises.length} writes to DynamoDB`);
-        await Promise.all(promises);
-    }
+    await importDevices(DEVICES_URL);
 
     return {
         statusCode: 200,
@@ -55,30 +17,4 @@ module.exports.handler = async () => {
             2
         ),
     };
-};
-
-const convertGpuToRequiredObject = (gpu) => {
-    return {
-        DeviceId: gpu.id,
-        Paying: gpu.paying,
-        Name: gpu.name,
-        Power: gpu.power,
-        Speeds: JSON.parse(gpu.speeds)
-    };
-}
-
-const saveToDynamoDb = async (items) => {
-    const putRequests = items.map(item => ({
-        PutRequest: {
-            Item: item
-        }
-    }));
-
-    const batchRequest = {
-        RequestItems: {
-            "Devices": putRequests
-        }
-    };
-
-    await dynamo.batchWrite(batchRequest).promise();
 };
